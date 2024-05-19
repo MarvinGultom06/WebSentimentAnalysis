5a1b7015067487da225b7729def6187a006ba385
JhonatanMarikros <jhonatanmarikros@gmail.com>
2024-05-20 00:21:50 +0700

    Update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import nltk
 from string import punctuation
 import re
 from nltk.corpus import stopwords
-
+import string
 
 # Download library nltk
 nltk.download('stopwords')
@@ -15,10 +15,9 @@ nltk.download('punkt')
 
 
 # Gunakan stopwords untuk menghilangkan kata-kata tidak berguna menggunakan inggris
-stop_words = stopwords.words('english')
-stop_words.remove('not')
+stop_words = set(stopwords.words("english"))
 
-# Buat instance aplikasi web dengan Flask
+# Buat aplikasi web dengan Flask
 app = Flask(__name__)
 
 try:
@@ -29,20 +28,26 @@ except FileNotFoundError:
 
 # Fungsi untuk membersihkan teks
 def preprocess_text(text):
-    # menghapus karakter selain huruf dan angka, dan membuang stopwords.
+    # menghapus karakter tanda baca,@, dan lain lain menggunakan library re.
     text = text.lower()
-    text = re.sub(r'[^\w\s]', '', text)  # Hapus karakter non-alfanumerik
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+','', text)
+    text = re.sub('<.*?>+','', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
     text = ' '.join([word for word in text.split() if word not in stop_words])
     return text
 
 # Fungsi untuk melakukan Vader sentiment analysis pada sebuah teks
 def vader_sentiment(text):
     sa = SentimentIntensityAnalyzer()
-    # menggunakan library Vader untuk memberi skor sentimen pada sebuah teks.
+    # menggunakan library Vader dengan polarity_scores untuk memberi skor sentimen pada sebuah teks.
     scores = sa.polarity_scores(text=text)
     return scores
 
-# Fungsi untuk menangani halaman utama aplikasi.
+# Route untuk web.
 @app.route('/')
 def my_form():
     if 'text' in data.columns:
@@ -51,7 +56,7 @@ def my_form():
         sample_text = "Error."
     return render_template('form.html', sample_text=sample_text)
 
-# Fungsi untuk menangani form yang disubmit pengguna pada halaman utama.
+# Fungsi untuk menangani form yang disubmit pengguna pada web.
 @app.route('/', methods=['POST'])
 def my_form_post():
     text1 = request.form['text1'].lower()
@@ -61,7 +66,7 @@ def my_form_post():
     sentiment_scores = vader_sentiment(processed_doc1)
     compound = round((1 + sentiment_scores['compound']) / 2, 2)
 
-    # Menghitung rata-rata populeritas dari teks
+    # Menghitung rata-rata dari teks
     average_popularity = round((sentiment_scores['pos'] + sentiment_scores['neg'] + sentiment_scores['neu']) / 3, 2)
 
     return render_template('form.html',
